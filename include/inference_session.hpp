a848c1068cf24ca8908c7523e896eaacda1537cf
Allan Ma <allan.ma@zozo.com>
2024-10-03 09:05:32 +0000

    chore: more progress on the way....

--- include/inference_session.hpp
+++ include/inference_session.hpp
@@ -8,6 +8,18 @@ public:
     InferenceSession(const std::shared_ptr<Ort::Env>& environment, const std::string& model_file);
     ~InferenceSession();
 
+    //! Get dimensions of input tensors we first passed to the session.
+    //! \note Value is not available until we run the model at least once.
+    [[nodiscard]] const std::vector<std::vector<int64_t>>& get_input_tensor_dimension()     const;
+
+    //! Get dimension of output tensors we got from infering the model at least once.
+    //! \note Value is not available until we run the model at least once.
+    [[nodiscard]] const std::vector<std::vector<int64_t>>& get_output_tensor_dimensions()   const;
+
+    //! Perform inference on the model
+    //! \param input_tensors input tensors.
+    [[nodiscard]] std::vector<Ort::Value> run(const std::vector<Ort::Value>& input_tensors);
+
 public:
     InferenceSession(const InferenceSession&)   = delete;
     InferenceSession(InferenceSession&&)        = delete;
@@ -19,8 +31,11 @@ private:
     std::shared_ptr<Ort::Env>       m_environment;
     std::unique_ptr<Ort::Session>   m_session;
 
+    std::vector<std::vector<int64_t>> m_input_tensor_dimension;
+    std::vector<std::vector<int64_t>> m_output_tensor_dimension;
+
     // I hate this so fucking much - we HAVE to pass an array of pointers that point to the input/output
     // names. This means we can't use std::string or anything like that since `sizeof != sizeof(uintptr_t)`
-    std::vector<const char*> m_input_names;
-    std::vector<const char*> m_output_names;
+    std::vector<const char*> m_input_node_names;
+    std::vector<const char*> m_output_node_names;
 };
